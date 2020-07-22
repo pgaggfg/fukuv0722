6e9672475278fd1fa88f854ae49a7820d25c72c0
Citrus.T <datdaisukemikancha@gmail.com>
2020-07-22 13:34:17 +0900

    提出

--- fukuv0722/Form1.cs
+++ fukuv0722/Form1.cs
@@ -16,5 +16,12 @@ namespace fukuv0722
         {
             InitializeComponent();
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text + 1);
+
+            label1.Top += int.Parse (textBox2.Text+ 1);
+        }
     }
 }
